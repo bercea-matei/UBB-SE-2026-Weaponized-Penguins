db0f49e063b41fb899da2e0cb65c6cf09094a617
razvanberbecar <razvan.berbecar@stud.ubbcluj.ro>
2026-03-25 20:35:48 +0200

    added Comment model, CommentsVotes model and VoteType enum

---
+++ src/Boards_WP.Data/Models/Comment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boards_WP.Data.Models
+{
+    internal class Comment
+    {
+    }
+}
---
+++ src/Boards_WP.Data/Models/CommentsVotes.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boards_WP.Data.Models;
+
+internal class CommentsVotes
+{
+}

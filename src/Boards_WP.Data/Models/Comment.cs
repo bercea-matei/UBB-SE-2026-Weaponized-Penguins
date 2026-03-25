33c33529d314896adb7d049e8b983a41e3c5c27f
razvanberbecar <razvan.berbecar@stud.ubbcluj.ro>
2026-03-25 20:39:04 +0200

    added Comment model, CommentsVotes model and VoteType enum

--- src/Boards_WP.Data/Models/Comment.cs
+++ src/Boards_WP.Data/Models/Comment.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Text;
 
 namespace Boards_WP.Data.Models
 {
-    internal class Comment
+    public class Comment
     {
+        private int commentID { get; set; }
+        private int postID { get; set; }
+        private int? parentID { get; set; }
+        private String description { get; set; } = String.Empty;
+        private int score { get; set; }
+        private DateTime creationTime { get; set; }
+        private int indentation { get; set; }
+        private Boolean isDeleted { get; set; }
+        private String ownerName { get; set; } = String.Empty;
+        private byte[]? ownerPicture { get; set; }
+        private VoteType userCurrentVote { get; set; }
+
+        public String getShareLink => $"boards://post/{postID}";
+
     }
 }
--- src/Boards_WP.Data/Models/CommentsVotes.cs
+++ src/Boards_WP.Data/Models/CommentsVotes.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace Boards_WP.Data.Models;
 
-internal class CommentsVotes
+public class CommentsVotes
 {
+    private int userID { get; set; }
+    private int commentID { get; set; }
+    private VoteType vote { get; set; } = VoteType.None;
+
 }

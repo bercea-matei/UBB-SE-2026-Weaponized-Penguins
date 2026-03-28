06da024a12fb72508fbec1fac628cbec4dbacd55
alexandrabochis <bochismariaalexandra@yahoo.com>
2026-03-28 20:16:01 +0200

    post and all related models

--- src/Boards_WP.Data/Models/Tag.cs
+++ src/Boards_WP.Data/Models/Tag.cs
@@ -1,10 +1,11 @@
 using System;
+using System.ComponentModel;
 
 namespace Boards_WP.Data.Models;
 
 public class Tag
 {
     public int TagID { get; set; }
-    public int CategoryID { get; set; }
+    public Category CategoryBelongingTo { get; set; }
     public String TagName { get; set; } = String.Empty; 
 }

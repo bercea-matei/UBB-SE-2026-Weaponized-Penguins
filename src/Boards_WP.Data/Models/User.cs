60fc4eaeedd51b40c48ded6d481670ddb3754b46
Alex <alexandru.bindiu@stud.ubbcluj.ro>
2026-03-28 17:59:06 +0200

    user updated

--- src/Boards_WP.Data/Models/User.cs
+++ src/Boards_WP.Data/Models/User.cs
@@ -5,9 +5,9 @@ namespace Boards_WP.Data.Models
     public class User
     {
         public int UserID { get; set; }
-        public String Username { get; set; };
-        public String Email { get; set; };
-        public String PasswordHash { get; set; };
+        public String Username { get; set; }
+        public String Email { get; set; }
+        public String PasswordHash { get; set; }
         public string AvatarUrl { get; set; }
         public string Bio { get; set; }
         public string Status { get; set; }

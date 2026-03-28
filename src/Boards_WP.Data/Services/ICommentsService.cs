75c52f25aede0cc5db9a2dfa4f56a9da7816d078
razvanberbecar <razvan.berbecar@stud.ubbcluj.ro>
2026-03-28 21:09:41 +0200

    mergin services into my develop branch (#27)
    * added ICommentsService and CommentsService. also, added the forgotted ownerID attribute in Comments Model
    
    * fixed increaseComment and decreaseComment in service
    
    * fixed increaseComment and decreaseComment
    
    * fixed file

---
+++ src/Boards_WP.Data/Services/ICommentsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Boards_WP.Data.Models;
+
+namespace Boards_WP.Data.Services;
+
+public interface ICommentsService
+{
+    public void addComment(Comment c);
+    public void softDeleteComment(Comment c, int userID);
+    public void increaseComment(Comment c, int currentUserID);
+    public void decreaseComment(Comment c, int currentUserID);
+    public List<Comment> getCommentsByPost(int postID, int currentUserID);
+
+}

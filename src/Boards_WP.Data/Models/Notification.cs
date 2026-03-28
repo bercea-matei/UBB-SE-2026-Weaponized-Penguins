cc01c8fe31061d2881fee075a8fee3446b8db7c9
Alex <alexandru.bindiu@stud.ubbcluj.ro>
2026-03-28 17:36:31 +0200

    notification and user models added

---
+++ src/Boards_WP.Data/Models/Notification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Boards_WP.Data.Models
+{
+    public class Notification
+    {
+        public int NotificationID { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int PostID { get; set; }
+        public int ReceiverID { get; set; }
+        public int ActorID { get; set; }
+        public NotificationType ActionType { get; set; }
+        public bool IsRead { get; set; }
+    }
+}

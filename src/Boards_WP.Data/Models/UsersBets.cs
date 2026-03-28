73b4d891434b9f9ed6fdad1e7161eaec456a2e67
Ionut2208 <beneionutmatei@gmail.com>
2026-03-29 00:11:06 +0200

    added missing models

---
+++ src/Boards_WP.Data/Models/UsersBets.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Boards_WP.Data.Models
+{
+    public class UsersBets
+    {
+        public User BettingUser { get; set; }
+        public Bet SelectedBet { get; set; }
+        public int Amount { get; set; }
+        public float Odd { get; set; }
+        public BetVote Vote { get; set; }
+
+    }
+}

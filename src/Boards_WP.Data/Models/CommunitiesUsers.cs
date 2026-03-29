f02d36cd94ba18ab688965ab7be550bee40ff15c
Bene Ionut <86953695+Ionut2208@users.noreply.github.com>
2026-03-29 12:50:11 +0300

    Ionut/communities users model+interfaces community and bet (#24)
    * added models + interfaces for repos
    
    * commit
    
    * added missing models
    
    * added required where it was needed + created ThemeColor enum and services interfaces

---
+++ src/Boards_WP.Data/Models/CommunitiesUsers.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Boards_WP.Data.Models
+{
+    public class CommunitiesUsers
+    {
+        public int CommunityID { get; set; }
+        public int UserID { get; set; }
+    }
+}

465b16f2a8bade152257f7db95e5ec525a0ac355
alexandrabochis <maria.alexandra.bochis@stud.ubbcluj.ro>
2026-03-29 23:19:43 +0300

    added post related repository interfaces (#29)
    Co-authored-by: alexandrabochis <bochismariaalexandra@yahoo.com>

---
+++ src/Boards_WP.Data/Repositories/Interfaces/IPostRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Boards_WP.Data.Models;
+
+namespace Boards_WP.Data.Repositories.Interfaces;
+
+public interface IPostsRepository
+{
+    public void AddPost(Post p);
+    public void DeletePost(int postID);
+    public void IncreaseScore(int postID);
+    public void DecreaseScore(int postID);
+    public void IncreaseCommentsNumber(int postID);
+    public Post GetPostByPostID(int postID);
+    public List<Post> GetPostsByCommunityIDs(int[] communityIDs);
+    public List<Post> GetPostExceptCommunityIDs(int[] communityIDs);
+
+}

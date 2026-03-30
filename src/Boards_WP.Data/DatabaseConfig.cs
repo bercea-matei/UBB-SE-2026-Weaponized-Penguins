025caa1fc22b6b21d6051ca7c5d3303ae17ce09e
razvanberbecar <razvan.berbecar@stud.ubbcluj.ro>
2026-03-31 02:01:12 +0300

    implemented rep and modified service

--- src/Boards_WP.Data/DatabaseConfig.cs
+++
@@ -1,7 +0,0 @@
-namespace Boards_WP.Data
-{
-    public static class DatabaseConfig
-    {
-        public const string ConnectionString = "Server=DESKTOP-1JCJMN6\\SQLEXPRESS;Database=Communities;Trusted_Connection=True;TrustServerCertificate=True;";
-    }
-}

2c62a8d46a36cc106df34398fb2a8d80038ae52b
Ionut2208 <beneionutmatei@gmail.com>
2026-03-30 23:02:03 +0300

    implemented repo and service for community

---
+++ src/Boards_WP.Data/Repositories/BetsRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boards_WP.Data.Repositories;
+
+public class BetsRepository : IBetsRepository
+{
+    public void AddBet(Bet b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddUserBet(UsersBets UserBet)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddUserTokens(UsersTokens userToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Bet> GetAllBetsSortedByDate()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Bet GetBetByID(int BetID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Bet> GetBetsByKeywords(string Keywords)
+    {
+        throw new NotImplementedException();
+    }
+
+    public UsersBets GetUserBetByID(int UserID, int BetID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<UsersBets> GetUserBetsByBet(int BetID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<UsersBets> GetUserBetsByUser(int UserID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public UsersTokens GetUserTokens(int UserID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveBet(int BetID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateBetAmounts(int BetID, int YesAmount, int NoAmount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateUserTokens(int UserID, int NewAmount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UserTokensExist(int UserID)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Boards_WP.Data/Services/BetsService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boards_WP.Data.Services;
+
+public class BetsService : IBetsService
+{
+    public (float YesOdd, float NoOdd) CalculateBetOdds(int BetID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public BetVote CheckBetCondition(int BetID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CreateBet(int UserID, Bet CreatedBet)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ExecuteActionsByBetResult(int UserID, int BetID, bool DidWin)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ExtractBetKeywords(string Input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Bet> GetAllBets()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Bet GetBetByID(int BetID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Bet> GetExpiredBets()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetUserTokenCount(int UserID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public float GetUserTokenFeeDiscount(int UserID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsSecretKey(string Input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void PlaceUserBet(int UserID, int BetID, int Amount, BetVote Vote)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ResolveBet(int BetID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Bet> SearchBetsByKeywords(string Keywords)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ValidateCreateBet(int UserID, Bet CreatedBet)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ValidatePlaceUserBet(int UserID, int BetID, int Amount, BetVote Vote)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool VerifySecretWordForBetOpening(string Input, int BetID)
+    {
+        throw new NotImplementedException();
+    }
+}

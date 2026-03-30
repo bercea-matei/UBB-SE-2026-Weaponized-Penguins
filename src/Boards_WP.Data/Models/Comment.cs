08196347f76484a9fb3b8c34625f933d98b2ff88
filiposzkar <filip.birliga@stud.ubbcluj.ro>
2026-03-31 01:00:10 +0300

    fix merge conflicts

--- src/Boards_WP.Data/Models/Comment.cs
+++ src/Boards_WP.Data/Models/Comment.cs
@@ -8,16 +8,16 @@ namespace Boards_WP.Data.Models
     public class Comment
     {
         public int CommentID { get; set; }
-        public Post Post { get; set; }
+        public Post ParentPost { get; set; }
         public Comment? ParentComment { get; set; }
         public User Owner { get; set; }
-        public String Description { get; set; } = string.Empty;
+        public String Description { get; set; } = String.Empty;
         public int Score { get; set; }
         public DateTime CreationTime { get; set; }
         public int Indentation { get; set; }
         public bool IsDeleted { get; set; }
         public VoteType UserCurrentVote { get; set; } = VoteType.None;
-        public string GetShareLink => $"boards://post/{Post?.PostID}";
+        public String GetShareLink => $"boards://post/{ParentPost.PostID}";
 
     }
 }
--- src/Boards_WP.Data/Models/Tag.cs
+++ src/Boards_WP.Data/Models/Tag.cs
@@ -7,6 +7,6 @@ public class Tag
 {
     public int TagID { get; set; }
     public Category CategoryBelongingTo { get; set; }
-    public string TagName { get; set; } = string.Empty; 
-    public string ColorHex {  get; set; } = string.Empty;
+    public String TagName { get; set; } = String.Empty;
+    public String getColorHex => CategoryBelongingTo.ColorHex;
 }

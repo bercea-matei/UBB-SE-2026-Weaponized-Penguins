cee6a88d26fa501977029fa6216d63951cf11239
razvanberbecar <razvan.berbecar@stud.ubbcluj.ro>
2026-03-31 13:35:35 +0300

    d

---
+++ src/Boards_WP.Data/DatabaseConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boards_WP.Data;
+
+public class DatabaseConfig
+{
+    public static string ConnectionString = @"Server=DESKTOP-1JCJMN6\SQLEXPRESS;Database=Communities;Integrated Security=True;TrustServerCertificate=True;";
+}

c452173301b347140522fa402ede8755b67ce3b3
filiposzkar <filip.birliga@stud.ubbcluj.ro>
2026-04-01 13:01:41 +0300

    Develop/filip (#56)
    * Added Views folder and HeaderView file
    
    * Added Header
    
    * Added Navigation Sidebar
    
    * Added Notifications Sidebar
    
    * Remove Transition
    
    * Added some comments
    
    * Added PostPreviewForm and Feed
    
    * Implemented PostPreviewForm and FeedView
    
    * Added FullPostView
    
    * Implemented full post view (without comments)
    
    * Added comments
    
    * Added comments
    
    * Implemented CommentView and modified Comment model (replaced init with set)
    
    * Implemented CommunityView and updated Tag, Post models (from init to set)
    
    * Implemented CommunityView and updated Tag, Post models (from init to set) (#40)
    
    * Added join, leave and create post buttons; implemented switching between community and feed views
    
    * Fix
    
    * Fix3
    
    * Replace inits and removed required
    
    * fix merge conflicts
    
    * Implemented CreatePostView
    
    * Implemented CreatePostView
    
    * Implemented CreateCommunityView
    
    * implemented the view model for Post Preview and implemented mvvm patter for Post Preview
    
    * Implemented full post view model and implemented mvvm design pattern for full post view
    
    ---------
    
    Co-authored-by: Alexandru Robert <robertalexandru2712@gmail.com>

--- src/Boards_WP/Views/CommentView.xaml.cs
+++ src/Boards_WP/Views/CommentView.xaml.cs
@@ -1,17 +1,21 @@
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
+using System;
 
 using Boards_WP.Data.Models;
+using Boards_WP.ViewModels;
+using Boards_WP.Views.Pages; 
+
+using Microsoft.UI.Input;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 
 namespace Boards_WP.Views
 {
     public sealed partial class CommentView : UserControl
-    {   
-
-        // just like we had a variable for posts in preview form, CommentData is an object of type Comment
+    {
+        
         public static readonly DependencyProperty CommentDataProperty =
-            DependencyProperty.Register("CommentData", typeof(Comment), typeof(CommentView),
-                new PropertyMetadata(null, OnDataChanged));
+            DependencyProperty.Register("CommentData", typeof(Comment), typeof(CommentView), new PropertyMetadata(null));
 
         public Comment CommentData
         {
@@ -24,74 +28,61 @@ namespace Boards_WP.Views
             this.InitializeComponent();
         }
 
-
-        // this function creates that nested look for the comments
-        // d is the comment which triggered the change (the comment that received a reply)
-        // e is a package which contains all the information about the change that just happened (it has e.NewValue and e.OldValue)
-        // e.OldValue is the data before change, e.NewValue is the data that was just assigned
-        private static void OnDataChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        
+        private FullPostViewModel GetParentViewModel()
         {
-            var control = (CommentView)d;
-            if (e.NewValue is Comment comment) // taking the new comment and reading its indentation
+            
+            if (App.Current is App myApp && myApp.m_window?.Content is Frame rootFrame)
             {
-                // adding the new line, so moving the comment a bit to the right based on its indendation
-                control.Margin = new Thickness(comment.Indentation * 35, 0, 0, 12);
-
-                if (comment.Indentation > 0) // if it is a reply, display the line
+                if (rootFrame.Content is FullPostView page)
                 {
-                    control.ThreadLine.Visibility = Visibility.Visible;
-                }
-                else
-                {
-                    control.ThreadLine.Visibility = Visibility.Collapsed;
+                    return page.ViewModel;
                 }
             }
+            return null;
         }
 
         private void Upvote_Click(object sender, RoutedEventArgs e)
         {
             if (CommentData == null) return;
-
-            if (CommentData.UserCurrentVote == VoteType.Like) // if the user already liked the comment, and they press the up-arrow again, we cancel their like
-            {
-                CommentData.Score--;
-                CommentData.UserCurrentVote = VoteType.None;
-            }
-            else if (CommentData.UserCurrentVote == VoteType.Dislike)  // if the user changed their mind, and went from dislike to like, we increment the post by 2
-            {
-                CommentData.Score += 2;
-                CommentData.UserCurrentVote = VoteType.Like;
-            }
-            else // if it is the first time they interact with the post, we simply increment the score by 1
-            {
-                CommentData.Score++;
-                CommentData.UserCurrentVote = VoteType.Like;
-            }
-
-            ScoreLabel.Text = CommentData.Score.ToString();
+            CommentData.Score++;
+            this.Bindings.Update();
         }
 
         private void Downvote_Click(object sender, RoutedEventArgs e)
         {
             if (CommentData == null) return;
+            CommentData.Score--;
+            this.Bindings.Update();
+        }
 
-            if (CommentData.UserCurrentVote == VoteType.Dislike)
+        
+        private void DeleteComment_Click(object sender, RoutedEventArgs e)
+        {
+            var viewModel = GetParentViewModel();
+            if (viewModel != null && CommentData != null)
             {
-                CommentData.Score++;
-                CommentData.UserCurrentVote = VoteType.None;
+                
+                viewModel.PostComments.Remove(this.CommentData);
             }
-            else if (CommentData.UserCurrentVote == VoteType.Like)
-            {
-                CommentData.Score -= 2;
-                CommentData.UserCurrentVote = VoteType.Dislike;
-            }
-            else
-            {
-                CommentData.Score--;
-                CommentData.UserCurrentVote = VoteType.Dislike;
-            }
+        }
 
-            ScoreLabel.Text = CommentData.Score.ToString();
+        
+        private void Grid_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            this.ProtectedCursor = InputSystemCursor.Create(InputSystemCursorShape.Hand);
+        }
+
+        private void Grid_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            this.ProtectedCursor = InputSystemCursor.Create(InputSystemCursorShape.Arrow);
+        }
+        private void Reply_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void PostReply_Click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }

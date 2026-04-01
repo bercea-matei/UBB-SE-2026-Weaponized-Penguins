8a5b47de7970e3a0cdb9dc2db1ed40307eb634b6
filiposzkar <filip.birliga@stud.ubbcluj.ro>
2026-04-01 17:54:11 +0300

    Implemented mvvm pattern for Community View

--- src/Boards_WP/Views/Pages/CommunityView.xaml.cs
+++ src/Boards_WP/Views/Pages/CommunityView.xaml.cs
@@ -1,213 +1,107 @@
 using System;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.IO;
-using System.Runtime.CompilerServices;
-
-using Boards_WP.Data.Models;
 
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Navigation;
 
+using Boards_WP.Data.Models;
 
 namespace Boards_WP.Views.Pages
-{   
-
-    // we will use INotifyPropertyChanged to tell the interface to change whenever a variable changes in C#
-    // the actual function which "shouts" the notification is at the bottom
-    public sealed partial class CommunityView : Page, INotifyPropertyChanged
+{
+    public sealed partial class CommunityView : Page
     {
-        public event PropertyChangedEventHandler PropertyChanged;
 
-        private Community _currentCommunity; // initially null
-        public Community CurrentCommunity { // the one the user is looking at
-            get => _currentCommunity;
-            set { _currentCommunity = value; OnPropertyChanged(); } // the UI will display the current community
+        // allowing other pages to pass a Community object into this page
+        public static readonly DependencyProperty CurrentCommunityProperty =
+            DependencyProperty.Register("CurrentCommunity", typeof(Community), typeof(CommunityView), new PropertyMetadata(null));
+
+        public Community CurrentCommunity
+        {
+            get => (Community)GetValue(CurrentCommunityProperty);
+            set => SetValue(CurrentCommunityProperty, value);
         }
 
+        public static readonly DependencyProperty IsMemberProperty =
+            DependencyProperty.Register("IsMember", typeof(bool), typeof(CommunityView), new PropertyMetadata(false));
 
-        public ObservableCollection<Post> CommunityPosts { get; set; } = new();
-
-
-        private bool _isMember = false; // initially false
-        
-        // when _isMember becomes true (so when the Join button gets clicked), the "set" code will run from the method IsMember
         public bool IsMember
         {
-            get => _isMember;
-            set  
-            {
-                _isMember = value;
-                OnPropertyChanged();
-                this.Bindings.Update(); // updates the button visibility
-            }
+            get => (bool)GetValue(IsMemberProperty);
+            set => SetValue(IsMemberProperty, value);
         }
 
-        // properties for XAML binding
-        public BitmapImage BannerImage => ConvertToBitmap(CurrentCommunity?.Banner);
-        public BitmapImage ProfileImage => ConvertToBitmap(CurrentCommunity?.Picture);
-        public string MemberCountText => $"{CurrentCommunity?.MembersNumber ?? 0} members";
-
-        public Visibility BoolToVisibility(bool isOpen) => isOpen ? Visibility.Visible : Visibility.Collapsed;
-        public Visibility InverseBoolToVisibility(bool isOpen) => isOpen ? Visibility.Collapsed : Visibility.Visible;
+        public ObservableCollection<Post> CommunityPosts { get; set; } = new();
 
         public CommunityView()
         {
             this.InitializeComponent();
         }
 
+        public BitmapImage BannerImage => ConvertToBitmap(CurrentCommunity?.Banner);
+        public BitmapImage ProfileImage => ConvertToBitmap(CurrentCommunity?.Picture);
+        public string MemberCountText => $"{CurrentCommunity?.MembersNumber ?? 0} members";
+        public Visibility JoinButtonVisibility => IsMember ? Visibility.Collapsed : Visibility.Visible;
+        public Visibility MemberActionsVisibility => IsMember ? Visibility.Visible : Visibility.Collapsed;
 
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
 
-            // 1) the parameter is a Community (either from Sidebar or CreateCommunityView)
             if (e.Parameter is Community community)
             {
                 CurrentCommunity = community;
-                CommunityPosts.Clear();
                 LoadDefaultPosts();
-
-                // check if I am the creator (Admin) to decide if I'm already a member
-                if (community.Admin?.Username == "@Me")
-                {
-                    IsMember = true;
-                }
-                else
-                {
-                    IsMember = false;
-                }
+                IsMember = (community.Admin?.Username == "@Me");
             }
-            // 2) the parameter is a Post (returning from CreatePostView)
-            else if (e.Parameter is Post newPost)
+            else if (e.Parameter is Post newPost)  // when a new post was created and needs to be displayed
             {
                 CurrentCommunity = newPost.ParentCommunity;
                 IsMember = true;
-
                 CommunityPosts.Add(newPost);
             }
             this.Bindings.Update();
         }
 
-        private void LoadDefaultPosts()
-        {
-            CommunityPosts.Add(new Post
-            {
-                PostID = 1,
-                Title = "Welcome!",
-                Owner = new User { Username = "@AlexBindiu" },
-                ParentCommunity = new Community { Name = "Computer Science", Admin = new User { Username = "System" } },
-                Score = 50,
-                CommentsNumber = 30,
-                CreationTime = new DateTime(2026, 3, 30),
-                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            });
-            CommunityPosts.Add(new Post
-            {
-                PostID = 1,
-                Title = "Community Rules",
-                Owner = new User { Username = "@Alexandra" },
-                ParentCommunity = new Community { Name = "Computer Science", Admin = new User { Username = "System" } },
-                Score = 34,
-                CommentsNumber = 10,
-                CreationTime = new DateTime(2026, 3, 30),
-                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            });
-            }
-
-        private void CreatePostButton_Click(object sender, RoutedEventArgs e)
-        {
-            // passing the CurrentCommunity so the form knows where it belongs
-            this.Frame.Navigate(typeof(CreatePostView), CurrentCommunity);
-        }
-
         private void JoinButton_Click(object sender, RoutedEventArgs e)
         {
             IsMember = true;
-            if (CurrentCommunity != null)
-            {
-                CurrentCommunity.MembersNumber++;
-                this.Bindings.Update(); 
-            }
+            if (CurrentCommunity != null) CurrentCommunity.MembersNumber++;
+            this.Bindings.Update();
         }
 
         private void LeaveButton_Click(object sender, RoutedEventArgs e)
         {
             IsMember = false;
-            if (CurrentCommunity != null)
-            {
-                CurrentCommunity.MembersNumber--;
-                this.Bindings.Update(); // refreshing the member count text
-            }
+            if (CurrentCommunity != null) CurrentCommunity.MembersNumber--;
+            this.Bindings.Update();
         }
 
-       
+        private void CreatePostButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(CreatePostView), CurrentCommunity);
+        }
 
-        // this function converts byte[] (from the images in the database) into BitmapImage, because XAML doesn't know byte[]
+     
         private BitmapImage ConvertToBitmap(byte[] data)
         {
             if (data == null || data.Length == 0) return null;
-
             var bitmap = new BitmapImage();
-            using (var ms = new MemoryStream(data)) // turning the image into a stream
+            using (var ms = new MemoryStream(data))
             {
-                var randomAccessStream = ms.AsRandomAccessStream();
-                bitmap.SetSource(randomAccessStream);
+                var stream = ms.AsRandomAccessStream();
+                bitmap.SetSource(stream);
             }
             return bitmap;
         }
 
-       
-        private void LoadCommunityPosts(int id)
+        private void LoadDefaultPosts()
         {
             CommunityPosts.Clear();
-            CommunityPosts.Add(new Post
-            {
-                PostID = 1,
-                Title = "Welcome!",
-                Owner = new User { Username = "@AlexBindiu" },
-                ParentCommunity = new Community { Name = "Computer Science", Admin = new User { Username = "System" } },
-                Score = 50,
-                CommentsNumber = 30,
-                CreationTime = new DateTime(2026, 3, 30),
-                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            });
-            CommunityPosts.Add(new Post
-            {
-                PostID = 1,
-                Title = "Community Rules",
-                Owner = new User { Username = "@Alexandra" },
-                ParentCommunity = new Community { Name = "Computer Science", Admin = new User { Username = "System" } },
-                Score = 34,
-                CommentsNumber = 10,
-                CreationTime = new DateTime(2026, 3, 30),
-                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            });
-        }
-
-        // this function opens the full view of a post in preview form inside a community
-        // sender: is the PostPreviewForm the user clicked on
-        // e: contains info about the click (not very relevant)
-
-        private void ClickOnPostFromCommunity(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
-        {
-            // converting the sender in a FrameworkElement, so we can see exactly which post in full view do we need to open (using the DataContext)
-            // will call this post "selectedPost"
-            if (sender is FrameworkElement fe && fe.DataContext is Post selectedPost)
-            {
-                // going to the FullPostView page of the selected post
-                this.Frame.Navigate(typeof(FullPostView), selectedPost);
-            }
-        }
-        
-        // this function sends a notification to Windows UI, announcing that the IsMember variable has changed
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            CommunityPosts.Add(new Post { Title = "Welcome!", Owner = new User { Username = "@Admin" }, Score = 50, Description = "Welcome to the community!" });
         }
     }
 }

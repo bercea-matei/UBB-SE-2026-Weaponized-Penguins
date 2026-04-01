f602efdfda993aad5c96a2eeb4f2c104d85d85f3
Ionut2208 <beneionutmatei@gmail.com>
2026-04-01 20:49:13 +0300

    begin createcommunityview

---
+++ src/Boards_WP/ViewModels/CreateCommunityViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Boards_WP.ViewModels;
+
+public partial class CreateCommunityViewModel : ObservableObject
+{
+    ICommunitiesService _communitiesService;
+
+    [ObservableProperty]
+    private string name;
+
+    [ObservableProperty]
+    private string description;
+
+    [ObservableProperty]
+    private bool isLoading;
+
+    [ObservableProperty]
+    private string errorMessage;
+
+    [ObservableProperty]
+    private byte[] picture;
+
+    [ObservableProperty]
+    private byte[] banner;
+
+
+    [RelayCommand]
+    private async Task CreateCommunity()
+    {
+        if (string.IsNullOrWhiteSpace(Name))
+        {
+            ErrorMessage = "Name is required";
+            return;
+        }
+
+        IsLoading = true;
+
+        try
+        {
+            Community CreatedCommunity = new Community()
+            {
+                Name = Name,
+                Description = Description,
+                Picture = Picture,
+                Banner = Banner,
+                Admin = App.CurrentUser
+            };
+
+            _communitiesService.AddCommunity(CreatedCommunity);
+
+            ErrorMessage = string.Empty;
+        }
+        catch (Exception)
+        {
+            ErrorMessage = "Something went wrong";
+        }
+        finally
+        {
+            IsLoading = false;
+        }
+    }
+}

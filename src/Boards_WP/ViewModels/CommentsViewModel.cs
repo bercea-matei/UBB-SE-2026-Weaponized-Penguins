cea67a9de0f4b0a8ad0506de23aa79fb2ea30e41
razvanberbecar <razvan.berbecar@stud.ubbcluj.ro>
2026-04-02 18:34:51 +0300

    Razvan/fixed comments backend logic, upvote downvote, add replies (#82)
    * gv
    
    * Fix comment backend logic: enable real DB comments, replies, upvoting, and DI connections
    
    * fixed replies. deleted tests so they dont interfere

--- src/Boards_WP/ViewModels/CommentsViewModel.cs
+++ src/Boards_WP/ViewModels/CommentsViewModel.cs
@@ -2,8 +2,12 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 using Boards_WP.Data.Models;
+using Boards_WP.Data.Services.Interfaces;
 
 using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Media;
 
 namespace Boards_WP.ViewModels
 {
@@ -25,18 +29,41 @@ namespace Boards_WP.ViewModels
         public IRelayCommand ToggleReplyCommand { get; }
         public IRelayCommand SubmitReplyCommand { get; }
 
+        private readonly ICommentsService _commentsService;
+        private readonly UserSession _userSession;
+
+        public SolidColorBrush UpvoteColor => CommentData?.UserCurrentVote == VoteType.Like 
+            ? new SolidColorBrush(Colors.OrangeRed) 
+            : new SolidColorBrush(Colors.Gray);
+
+        public SolidColorBrush DownvoteColor => CommentData?.UserCurrentVote == VoteType.Dislike 
+            ? new SolidColorBrush(Colors.CornflowerBlue) 
+            : new SolidColorBrush(Colors.Gray);
+
         public CommentViewModel(Comment comment)
         {
             CommentData = comment;
+            _commentsService = App.Services?.GetService<ICommentsService>();
+            _userSession = App.Services?.GetService<UserSession>();
 
             UpvoteCommand = new RelayCommand(() => {
-                CommentData.Score++;
-                OnPropertyChanged(nameof(CommentData));
+                if (_commentsService != null && _userSession != null)
+                {
+                    _commentsService.IncreaseScore(CommentData, _userSession.CurrentUser.UserID);
+                    OnPropertyChanged(nameof(CommentData));
+                    OnPropertyChanged(nameof(UpvoteColor));
+                    OnPropertyChanged(nameof(DownvoteColor));
+                }
             });
 
             DownvoteCommand = new RelayCommand(() => {
-                CommentData.Score--;
-                OnPropertyChanged(nameof(CommentData));
+                if (_commentsService != null && _userSession != null)
+                {
+                    _commentsService.DecreaseScore(CommentData, _userSession.CurrentUser.UserID);
+                    OnPropertyChanged(nameof(CommentData));
+                    OnPropertyChanged(nameof(UpvoteColor));
+                    OnPropertyChanged(nameof(DownvoteColor));
+                }
             });
 
             ToggleReplyCommand = new RelayCommand(() => IsReplyAreaVisible = !IsReplyAreaVisible);

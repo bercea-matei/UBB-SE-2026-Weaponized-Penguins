30918b508fada54a74448bcd4df0b865ce3c1a51
Alex <alexandru.bindiu@stud.ubbcluj.ro>
2026-04-03 00:42:38 +0300

    .

--- src/Boards_WP/ViewModels/CommunityViewModel.cs
+++ src/Boards_WP/ViewModels/CommunityViewModel.cs
@@ -21,6 +21,10 @@ namespace Boards_WP.ViewModels
         private readonly IPostsService _postsService;
         private readonly ICommunitiesService _communitiesService;
         private readonly UserSession _userSession;
+        private readonly MainViewModel _mainViewModel;
+
+        public MainViewModel MainViewModel => _mainViewModel;
+
         private readonly Action<Community> _navigateToCreatePost;
         private readonly Action<Community> _navigateToEditCommunity;
 
@@ -63,6 +67,7 @@ public ObservableCollection<PostPreviewViewModel> CommunityPosts { get; } = new(
             _postsService = App.Services?.GetService<IPostsService>();
             _communitiesService = App.Services?.GetService<ICommunitiesService>();
             _userSession = App.Services?.GetService<UserSession>();
+            _mainViewModel = App.Services?.GetService<MainViewModel>();
             _navigateToCreatePost = navigateToCreatePost;
             _navigateToEditCommunity = navigateToEditCommunity;
         }

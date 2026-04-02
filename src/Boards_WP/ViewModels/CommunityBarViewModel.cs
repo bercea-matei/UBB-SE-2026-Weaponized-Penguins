0403d3cd805277d08840ee7b502f98fe836db57b
Bercea Matei <140618127+bercea-matei@users.noreply.github.com>
2026-04-02 15:10:45 -0700

    Matei/200batch/update (#95)
    * update: dynamic my-communities
    
    made the left side bar with my communities update with info from the db and in real time.
    
    also updated the code for join leave functionalities to appropiately add/remove a user from a community (or dissalow in case of admins)
    
    * update: notification pagination

--- src/Boards_WP/ViewModels/CommunityBarViewModel.cs
+++ src/Boards_WP/ViewModels/CommunityBarViewModel.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Text;
-
-using Boards_WP.Data.Models;
-using Boards_WP.Data.Services; // Ensure this matches your Service namespace
-using Boards_WP.ViewModels;
+﻿using Boards_WP.ViewModels;
 using Boards_WP.Views.Pages;
 using Boards_WP;
 
@@ -15,11 +8,12 @@ public partial class CommunityBarViewModel
 {
     private readonly INavigationService _navigationService;
     private readonly ICommunitiesService _communitiesService;
-    private readonly FeedViewModel _feedViewModel; // Injected Singleton
+    private readonly FeedViewModel _feedViewModel;
     private readonly MainViewModel _mainViewModel;
     private readonly UserSession _userSession;
 
     public MainViewModel MainViewModel => _mainViewModel;
+
 
     public ObservableCollection<Community> Communities { get; } = new();
 

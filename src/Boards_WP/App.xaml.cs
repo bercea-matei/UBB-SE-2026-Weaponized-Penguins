8130f0f58d425288642bd364b3c1c0c3cc48fdde
razvanberbecar <razvan.berbecar@stud.ubbcluj.ro>
2026-04-03 03:22:17 +0300

    merged

--- src/Boards_WP/App.xaml.cs
+++ src/Boards_WP/App.xaml.cs
@@ -45,7 +45,7 @@ public partial class App : Application
     {
         var services = new ServiceCollection();
 
-        string connectionString = @"Data Source = DESKTOP\SQLEXPRESS;Database=Communities;Initial Catalog=Communities;Integrated Security=True;Encrypt=False;TrustServerCertificate=True;";
+        string connectionString = @"Data Source = DESKTOP-1JCJMN6\SQLEXPRESS;Database=Communities;Initial Catalog=Communities;Integrated Security=True;Encrypt=False;TrustServerCertificate=True;";
         services.AddSingleton(connectionString);
 
 
